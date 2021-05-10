e336cd51c9f4f9050f1e979b346616c269d889ac
Saad BENDAOUD <saad.benda@gmail.com>
2021-05-10 20:29:08 +0200

    checkpoint

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DropdownModule} from 'primeng/dropdown';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { DashboardComponent } from './Component/dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
