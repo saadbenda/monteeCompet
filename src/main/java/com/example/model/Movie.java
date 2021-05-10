653696b18cc9c80a6d458587bdca693ee9cba5f2
Saad BENDAOUD <saad.benda@gmail.com>
2021-05-10 10:37:54 +0200

    move to SpringBoot

---
+++ src/main/java/com/example/model/Movie.java
@@ -0,0 +1,39 @@
+package model;
+
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity
+public class Movie {
+
+    @Id
+    private int id;
+    private String name;
+    private String director;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
